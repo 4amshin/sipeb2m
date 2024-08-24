ee68502f8bd93a4cc9185256608241545f82b4fc
4amshin <shinmendenz@gmail.com>
2024-08-25 07:57:32 +0800

    Menambahkan Logo UNCP

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,9 +1,12 @@
-<div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
-    <div class="mb-2 mb-md-0">
+<div class="container-xxl d-flex flex-wrap justify-content-between align-items-center py-2 flex-md-row flex-column">
+    <div class="mb-2 mb-md-0 d-flex align-items-center">
         ©
         <script>
             document.write(new Date().getFullYear());
         </script>
-        SIPEB2M
+        <div class="uncp-logo ms-2">
+            <img src="{{ asset('assets/img/uncp-logo.svg') }}" alt="Logo UNCP" height="30">
+        </div>
     </div>
 </div>
+
